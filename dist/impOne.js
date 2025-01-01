1a54906cad826d41f9e6d70a0e01a85cc7f3b83f
Saurabhkumar Sharma <saurabhk.sharma1410@gmail.com>
2025-01-01 20:53:42 +0530

    basics of TS

---
+++ dist/impOne.js
@@ -0,0 +1,15 @@
+"use strict";
+// let random:any;  //avoid
+// let a:number=24;    
+// let names:string;
+// let b=<number>24;    
+// let surname:number|string;
+// let lastname=<number|string>23;
+// const func=(a:number,b:number):number=>{    return a*b; }
+// function fn(n:number,m:number):number { return n*m;}
+// type newDataType=string|number;
+// let a:newDataType=24;
+// let b=<newDataType>"hello";
+// type funcDataType=(a:number,b:number)=>number;
+// const newFunc:funcDataType=(a,b)=>(a*b);
+// const func:funcDataType=function(a,b){return a*b}; // can't do this without storing into a variable
